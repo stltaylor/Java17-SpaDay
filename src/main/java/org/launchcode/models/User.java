ac7e12ecfa66c20d49d04271afabadb62365840e
unknown <crtaylorlovesstl@gmail.com>
2024-05-13 20:19:19 -0500

    valid

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,19 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank
+    @Size(min = 5, max = 15, message = "Must be between 5 and 15 chars")
     private String username;
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Must be 6 chars long at least.")
     private String password;
 
     public User() {
